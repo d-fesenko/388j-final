4a77e3382c5ef8d0bfb565991d1c7825ab8236a8
d-fesenko <dfesenko@terpmail.umd.edu>
2024-04-24 17:07:16 -0400

    users stored in mongo

--- app.py
+++ app.py
@@ -1,22 +1,39 @@
 from flask import Flask, url_for, session, request, redirect, jsonify, render_template
+from flask_login import current_user, login_required, login_user, logout_user, LoginManager
+from flask_login import UserMixin
+from flask_mongoengine import MongoEngine
 import requests
 import re
 from urllib.parse import urlencode
 from config import STEAM_API_KEY
 
+db = MongoEngine()
+login_manager = LoginManager()
+
+
 app = Flask(__name__)
-app.secret_key = b'\x9dGP\x1dq\xec=\x05_\xfd=(\xd3q"a'
+
+app.config['SECRET_KEY'] = b'\x9dGP\x1dq\xec=\x05_\xfd=(\xd3q"a'
+app.config['MONGODB_HOST'] = 'mongodb+srv://dfesenko:daniel2004@cluster0.ac3iyvb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
+
+    
+db.init_app(app)
+login_manager.init_app(app)
+
+
+
 
 @app.route('/')
 def index():
-    if 'userData' in session:
-        userdata = session['userData']
-    else:
-        userdata = {}
-    return render_template('index.html', userdata = userdata)
+    if current_user.is_authenticated:
+        print(current_user)
+    return render_template('index.html', userdata = {})
 
 @app.route('/login')
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index')) #this should probably redirect to the account page, once we make one 
+    
     login_url_params = {
         'openid.ns': 'http://specs.openid.net/auth/2.0',
         'openid.mode': 'checkid_setup',
@@ -28,10 +45,14 @@ def login():
     steam_login_url = 'https://steamcommunity.com/openid/login?' + urlencode(login_url_params)
     return redirect(steam_login_url)
 
+@app.route('/logout')
+def logout():
+    session.clear()
+    return redirect(url_for('index'))
+
 @app.route('/process-openid')
 def process_openid():
     # Extract the necessary parameters from query string
-    print(request.args)
     openid_params = {
         'openid.assoc_handle': request.args.get('openid_assoc_handle'),
         'openid.signed': request.args.get('openid_signed'),
@@ -40,9 +61,7 @@ def process_openid():
         'openid.mode': 'check_authentication',
     }
 
-    
-
-    response = requests.post('https://steamcommunity.com/openid/login')
+    response = requests.post('https://steamcommunity.com/openid/login', params=openid_params)
     if response:
         match = re.search(r'https://steamcommunity.com/openid/id/(\d+)', request.args.get('openid.claimed_id', ''))
         if match:
@@ -54,21 +73,22 @@ def process_openid():
             if api_response.get('response') and api_response['response'].get('players'):
                 userData = api_response['response']['players'][0]
 
-                # Set user data in session
-                session['logged_in'] = True
-                session['userData'] = {
-                    'steam_id': userData['steamid'],
-                    'name': userData['personaname'],
-                    'avatar': userData['avatarmedium'],
-                }
-                return redirect(url_for('dashboard'))
-    return "Error: Unable to validate your request"
+                user = User(steamid=userData['steamid'], name=userData['personaname'], avatar=userData['avatarmedium'])
+                user.save() 
+                login_user(user)
 
-@app.route('/dashboard')
-def dashboard():
-    if session.get('logged_in'):
-        return redirect(url_for('index'))
-    return redirect(url_for('login'))
+                return redirect(url_for('index'))
+    return "Error: Unable to validate your request" #This should prolly never happen
 
-if __name__ == '__main__':
-    app.run(debug=True)
+@login_manager.user_loader
+def load_user(user_id):
+    return User.objects(pk=user_id).first()
+
+class User(db.Document, UserMixin):
+    steamid = db.StringField(required = True)
+    name = db.StringField(required = True)
+    avatar = db.StringField(required = True) #The URL for the avatar image, not the image itself
+
+    # Implement the get_id method required by Flask-Login
+    def get_id(self):
+        return str(self.id)
--- forms.py
+++ forms.py
@@ -14,41 +14,9 @@ from wtforms.validators import (
 )
 
 
-from .models import User
-
 
 class SearchForm(FlaskForm):
     search_query = StringField(
         "Query", validators=[InputRequired(), Length(min=1, max=100)]
     )
     submit = SubmitField("Search")
-
-
-class RegistrationForm(FlaskForm):
-    username = StringField("Username", validators=[InputRequired()])
-    password = PasswordField("Password", validators=[InputRequired()])
-    confirm_password = PasswordField(
-        "Confirm Password", validators=[InputRequired(), EqualTo("password")]
-    )
-
-    submit = SubmitField("Sign Up")
-
-    def validate_username(self, username):
-        user = User.objects(username=username.data).first()
-        if user is not None:
-            raise ValidationError("Username is taken")
-
-
-class LoginForm(FlaskForm): 
-    username = StringField('Username', validators=[DataRequired()])
-    password = PasswordField('Password', validators=[DataRequired()])
-    submit = SubmitField('Log In')
-
-class UpdateUsernameForm(FlaskForm):
-    username = StringField('Username', validators=[InputRequired(), Length(min=1, max=40)])
-    submit_username = SubmitField('Update Username') 
-
-    def validate_username(self, username):
-        user = User.objects(username=username.data).first()
-        if user:
-            raise ValidationError("Username is taken")
